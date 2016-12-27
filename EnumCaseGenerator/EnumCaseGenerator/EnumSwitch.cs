ec3ee4ea481120f1a10f803eca236578f9adcc38
Simon Mourier <simon_mourier@hotmail.com>
2016-12-27 08:51:23 +0100

    Work in progress for V2

---
+++ EnumCaseGenerator/EnumCaseGenerator/EnumSwitch.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EnumCaseGenerator
+{
+    public class EnumSwitch
+    {
+        private List<IFieldSymbol> _existingFields = new List<IFieldSymbol>();
+        private List<IFieldSymbol> _missingFields = new List<IFieldSymbol>();
+
+        private EnumSwitch(SwitchStatementSyntax node, ITypeSymbol type)
+        {
+            Node = node;
+            EnumType = type;
+            _missingFields.AddRange(EnumType.GetMembers().OfType<IFieldSymbol>());
+
+            for (int i = 0; i < node.Sections.Count; i++)
+            {
+                foreach (var label in node.Sections[i].Labels)
+                {
+                    if (label is DefaultSwitchLabelSyntax)
+                    {
+                        DefaultCase = (DefaultSwitchLabelSyntax)label;
+                        DefaultCaseIsLast = i == node.Sections.Count - 1;
+                        continue;
+                    }
+
+                    var caseLabel = label as CaseSwitchLabelSyntax;
+                    if (caseLabel == null)
+                        continue;
+
+                    var member = caseLabel.Value as MemberAccessExpressionSyntax;
+                    if (member == null)
+                        continue;
+
+                    var name = member.Name as IdentifierNameSyntax;
+                    if (name == null || name.Identifier == null || string.IsNullOrWhiteSpace(name.Identifier.Text))
+                        continue;
+
+                    var field = _missingFields.FirstOrDefault(f => f.Name == name.Identifier.Text);
+                    if (field == null)
+                        continue;
+
+                    _existingFields.Add(field);
+                    _missingFields.Remove(field);
+                }
+            }
+
+            var sorted = new List<IFieldSymbol>(_existingFields);
+            sorted.Sort(new FieldComparer(true));
+            IsSortedAlphabetically = IsSame(_existingFields, sorted);
+
+            sorted.Sort(new FieldComparer(false));
+            IsSortedByValues = IsSame(_existingFields, sorted);
+        }
+
+        public SwitchStatementSyntax Node { get; }
+        public ITypeSymbol EnumType { get; }
+        public bool IsSortedAlphabetically { get; }
+        public bool IsSortedByValues { get; }
+        public bool? DefaultCaseIsLast { get; }
+
+        public DefaultSwitchLabelSyntax DefaultCase { get; }
+        public bool HasDefaultCase => DefaultCase != null;
+        public bool HasMissingCases => _missingFields.Count > 0;
+        public IReadOnlyList<IFieldSymbol> ExistingFields => _existingFields;
+        public IReadOnlyList<IFieldSymbol> MissingFields => _missingFields;
+
+        public static EnumSwitch Parse(SemanticModel model, SwitchStatementSyntax node)
+        {
+            if (model == null)
+                throw new ArgumentNullException(nameof(model));
+
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+
+            // get type and check it's an enum
+            var ti = model.GetTypeInfo(node.Expression);
+            var type = ti.ConvertedType;
+            if (type == null || type.TypeKind != TypeKind.Enum)
+                return null;
+
+            // is it a flags (multi-valued) attribute? we don't want to handle those
+            var flags = type.GetAttributes().Any(a => a.AttributeClass?.ContainingNamespace?.Name == typeof(FlagsAttribute).Namespace && a.AttributeClass?.Name == typeof(FlagsAttribute).Name);
+            if (flags)
+                return null;
+
+            return new EnumSwitch(node, type);
+        }
+
+        private static bool IsSame<T>(List<T> first, List<T> second)
+        {
+            if (first.Count != second.Count)
+                return false;
+
+            for (int i = 0; i < first.Count; i++)
+            {
+                if (!EqualityComparer<T>.Default.Equals(first[i], second[i]))
+                    return false;
+            }
+            return true;
+        }
+
+        private class FieldComparer : IComparer<IFieldSymbol>
+        {
+            private bool _alpha;
+
+            public FieldComparer(bool alpha)
+            {
+                _alpha = alpha;
+            }
+
+            public int Compare(IFieldSymbol x, IFieldSymbol y)
+            {
+                if (x != null && y != null)
+                {
+                    if (_alpha)
+                    {
+                        if (x.Name != null)
+                            return x.Name.CompareTo(y.Name);
+                    }
+                    else
+                    {
+                        // enum value are comparable (hopefully)
+                        var cx = x.ConstantValue as IComparable;
+                        if (cx != null)
+                        {
+                            try
+                            {
+                                return cx.CompareTo(y.ConstantValue);
+                            }
+                            catch
+                            {
+                                // hmm... maybe this could happen if types are not compatible, so we do nothing
+                            }
+                        }
+                    }
+                }
+                return 0; // don't know
+            }
+        }
+    }
+}
--- EnumCaseGenerator/EnumCaseGenerator/GeneratorRefactoring.cs
+++ EnumCaseGenerator/EnumCaseGenerator/GeneratorRefactoring.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Composition;
 using System.Collections.Generic;
+using System.Composition;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
-using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
 
 namespace EnumCaseGenerator
 {
@@ -21,100 +18,96 @@ namespace EnumCaseGenerator
         public sealed async override Task ComputeRefactoringsAsync(CodeRefactoringContext context)
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-            var switchNode = root.FindNode(context.Span) as SwitchStatementSyntax;
+            var node = root.FindNode(context.Span);
+            if (node == null)
+                return;
+
+            var switchNode = node.FirstAncestorOrSelf<SwitchStatementSyntax>();
             if (switchNode == null || switchNode.Expression == null)
                 return;
 
             var model = context.Document.GetSemanticModelAsync().Result;
+            var enumSwitch = EnumSwitch.Parse(model, switchNode);
+            if (enumSwitch == null)
+                return; // unhandled
 
-            // get type and check it's an enum
-            var ti = model.GetTypeInfo(switchNode.Expression);
-            var type = ti.ConvertedType;
-            if (type == null || type.TypeKind != TypeKind.Enum)
-                return;
-            
-            // is it a flags (multi-valued) attribute? we don't want to handle those
-            var flags = type.GetAttributes().Any(at => at.AttributeClass?.ContainingNamespace?.Name == typeof(FlagsAttribute).Namespace && at.AttributeClass?.Name == typeof(FlagsAttribute).Name);
-            if (flags)
-                return;
-
-            bool hasDefault;
-            if (!GetMissingValues(switchNode, type, out hasDefault).Any())
-                return;
-
-            var action = CodeAction.Create(
-                "Ensure all cases for enum type '" + type.Name + "'",
-                ct => PopulateEnumSwitch(context, switchNode, type, ct, false));
-
-            context.RegisterRefactoring(action);
-
-            if (!hasDefault)
+            CodeAction action;
+            if (enumSwitch.HasMissingCases)
             {
                 action = CodeAction.Create(
-                    "Ensure all cases for enum type '" + type.Name + "' with default",
-                    ct => PopulateEnumSwitch(context, switchNode, type, ct, true));
+                    "Ensure all cases for enum type '" + enumSwitch.EnumType.Name + "'",
+                    ct => PopulateEnumSwitch(context, ct, enumSwitch, false));
+                context.RegisterRefactoring(action);
+            }
 
+            if (!enumSwitch.HasDefaultCase)
+            {
+                action = CodeAction.Create(
+                    "Ensure all cases for enum type '" + enumSwitch.EnumType.Name + "' with default",
+                    ct => PopulateEnumSwitch(context, ct, enumSwitch, true));
+                context.RegisterRefactoring(action);
+            }
+
+            if (!enumSwitch.IsSortedAlphabetically)
+            {
+                action = CodeAction.Create(
+                    "Sort switch cases alphabetically",
+                    ct => SortEnumSwitch(context, ct, enumSwitch, true, false));
+                context.RegisterRefactoring(action);
+            }
+
+            if (!enumSwitch.IsSortedByValues)
+            {
+                action = CodeAction.Create(
+                    "Sort switch cases by enum value",
+                    ct => SortEnumSwitch(context, ct, enumSwitch, false, true));
+                context.RegisterRefactoring(action);
+            }
+
+            if (enumSwitch.HasDefaultCase && !enumSwitch.DefaultCaseIsLast.GetValueOrDefault())
+            {
+                action = CodeAction.Create(
+                    "Move default case to end",
+                    ct => MoveDefaultCaseToEndOfEnumSwitch(context, ct, enumSwitch));
                 context.RegisterRefactoring(action);
             }
         }
 
-        private static IReadOnlyList<string> GetExistingValues(SwitchStatementSyntax switchNode, out bool hasDefault)
+        private async Task<Document> RebuildSections(CodeRefactoringContext context, CancellationToken cancellationToken, EnumSwitch enumSwitch, IEnumerable<SwitchSectionSyntax> sections)
         {
-            hasDefault = false;
-            var list = new List<string>();
-            foreach (var section in switchNode.Sections)
-            {
-                // we don't want DefaultSwitchLabelSyntax
-                foreach (var label in section.Labels)
-                {
-                    if (label is DefaultSwitchLabelSyntax)
-                    {
-                        hasDefault = true;
-                        continue;
-                    }
+            var newSwitch = SyntaxFactory.SwitchStatement(
+                enumSwitch.Node.SwitchKeyword,
+                enumSwitch.Node.OpenParenToken,
+                enumSwitch.Node.Expression,
+                enumSwitch.Node.CloseParenToken,
+                enumSwitch.Node.OpenBraceToken,
+                SyntaxFactory.List(sections),
+                enumSwitch.Node.CloseBraceToken);
 
-                    var caseLabel = label as CaseSwitchLabelSyntax;
-                    if (caseLabel == null)
-                        continue;
-
-                    var member = caseLabel.Value as MemberAccessExpressionSyntax;
-                    if (member == null)
-                        continue;
-
-                    var name = member.Name as IdentifierNameSyntax;
-                    if (name == null || name.Identifier == null || string.IsNullOrWhiteSpace(name.Identifier.Text))
-                        continue;
-
-                    list.Add(name.Identifier.Text);
-                }
-            }
-            return list;
+            var oldRoot = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var newRoot = oldRoot.ReplaceNode(enumSwitch.Node, newSwitch);
+            return context.Document.WithSyntaxRoot(newRoot);
         }
 
-        private static IReadOnlyList<string> GetMissingValues(SwitchStatementSyntax switchNode, ITypeSymbol enumType, out bool hasDefault)
+        private async Task<Document> SortEnumSwitch(CodeRefactoringContext context, CancellationToken cancellationToken, EnumSwitch enumSwitch, bool byAlpha, bool byValue)
         {
-            var list = new List<string>();
-            var existing = GetExistingValues(switchNode, out hasDefault);
-            var missing = new List<string>();
-            foreach (var field in enumType.GetMembers().OfType<IFieldSymbol>())
-            {
-                if (existing.Contains(field.Name))
-                    continue;
-
-                list.Add(field.Name);
-            }
-            return list;
+            return await RebuildSections(context, cancellationToken, enumSwitch, enumSwitch.Node.Sections);
         }
 
-        private async Task<Document> PopulateEnumSwitch(CodeRefactoringContext context, SwitchStatementSyntax switchNode, ITypeSymbol enumType, CancellationToken cancellationToken, bool withDefault)
+        private async Task<Document> MoveDefaultCaseToEndOfEnumSwitch(CodeRefactoringContext context, CancellationToken cancellationToken, EnumSwitch enumSwitch)
         {
-            var newSections = new List<SwitchSectionSyntax>(switchNode.Sections);
-            bool hasDefault;
-            foreach (string name in GetMissingValues(switchNode, enumType, out hasDefault))
+            return await RebuildSections(context, cancellationToken, enumSwitch, enumSwitch.Node.Sections);
+        }
+
+        private async Task<Document> PopulateEnumSwitch(CodeRefactoringContext context, CancellationToken cancellationToken, EnumSwitch enumSwitch, bool withDefault)
+        {
+            var newSections = new List<SwitchSectionSyntax>();
+
+            foreach (var field in enumSwitch.MissingFields)
             {
                 var access = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
-                    SyntaxFactory.IdentifierName(enumType.Name),
-                    SyntaxFactory.IdentifierName(name));
+                    SyntaxFactory.IdentifierName(enumSwitch.EnumType.Name),
+                    SyntaxFactory.IdentifierName(field.Name));
                 var label = SyntaxFactory.CaseSwitchLabel(access);
                 var statement = SyntaxFactory.BreakStatement();
                 var newSection = SyntaxFactory.SwitchSection(
@@ -123,7 +116,7 @@ namespace EnumCaseGenerator
                 newSections.Add(newSection);
             }
 
-            if (!hasDefault && withDefault)
+            if (!enumSwitch.HasDefaultCase && withDefault)
             {
                 var label = SyntaxFactory.DefaultSwitchLabel();
                 var statement = SyntaxFactory.BreakStatement();
@@ -133,19 +126,12 @@ namespace EnumCaseGenerator
                 newSections.Add(newSection);
             }
 
-            var oldRoot = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            foreach (var section in enumSwitch.Node.Sections)
+            {
+                newSections.Add(section);
+            }
 
-            var newSwitch = SyntaxFactory.SwitchStatement(
-                switchNode.SwitchKeyword,
-                switchNode.OpenParenToken,
-                switchNode.Expression,
-                switchNode.CloseParenToken,
-                switchNode.OpenBraceToken,
-                SyntaxFactory.List(newSections),
-                switchNode.CloseBraceToken);
-
-            var newRoot = oldRoot.ReplaceNode(switchNode, newSwitch);
-            return context.Document.WithSyntaxRoot(newRoot);
+            return await RebuildSections(context, cancellationToken, enumSwitch, newSections);
         }
     }
 }
