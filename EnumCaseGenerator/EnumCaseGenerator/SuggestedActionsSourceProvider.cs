b26ae9dd4cdba8627293ba38703a137d2c0651fa
Simon Mourier <simon_mourier@hotmail.com>
2017-03-07 07:22:21 +0100

    Updated id

---
+++ EnumCaseGenerator/EnumCaseGenerator/SuggestedActionsSourceProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace EnumCaseGenerator
+{
+    [Export(typeof(ISuggestedActionsSourceProvider)), ContentType("text"), Name("EnumCaseGenerator")]
+    public class SuggestedActionsSourceProvider : ISuggestedActionsSourceProvider
+    {
+        public ISuggestedActionsSource CreateSuggestedActionsSource(ITextView textView, ITextBuffer textBuffer)
+        {
+            return new SuggestedActionsSource();
+        }
+
+        private class SuggestedActionsSource : ISuggestedActionsSource
+        {
+            public event EventHandler<EventArgs> SuggestedActionsChanged;
+
+            public void Dispose()
+            {
+            }
+
+            public IEnumerable<SuggestedActionSet> GetSuggestedActions(ISuggestedActionCategorySet requestedActionCategories, SnapshotSpan range, CancellationToken cancellationToken)
+            {
+                yield break;
+            }
+
+            public Task<bool> HasSuggestedActionsAsync(ISuggestedActionCategorySet requestedActionCategories, SnapshotSpan range, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(GetSuggestedActions(requestedActionCategories, range, cancellationToken).Any());
+            }
+
+            public bool TryGetTelemetryId(out Guid telemetryId)
+            {
+                telemetryId = Guid.Empty;
+                return false;
+            }
+        }
+    }
+}
